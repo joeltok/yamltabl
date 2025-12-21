1237e54dcd4b15a357e895548e3809b5c6a45792
Joel Tok <jttctc@gmail.com>
2025-12-21 16:45:15 +0800

    add skeleton vscode extension

---
+++ packages/vscode-extension/tools/build.js
@@ -0,0 +1,31 @@
+import esbuild from 'esbuild';
+
+const production = process.argv.includes('--production');
+const watch = process.argv.includes('--watch');
+
+async function main() {
+	const ctx = await esbuild.context({
+    entryPoints: ['packages/vscode-extension/src/index.ts'],
+    outfile: 'dist/packages/vscode-extension/lib/index.js',
+		bundle: true,
+		format: 'cjs',
+    target: 'es2020',
+		minify: production,
+		sourcemap: !production,
+		sourcesContent: false,
+		platform: 'node',
+		external: ['vscode'],
+		logLevel: 'info',
+	});
+	if (watch) {
+		await ctx.watch();
+	} else {
+		await ctx.rebuild();
+		await ctx.dispose();
+	}
+}
+
+main().catch(e => {
+	console.error(e);
+	process.exit(1);
+});
